c3fff3cac089a927db7675d113ead088daecee5f
Sun, Yiling <Yiling.Sun@emc.com>
2018-04-15 00:39:38 +0800

    working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map
+ } from 'rxjs/operators';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +22,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  model = 50;
+  breadcrumbItems = [{
+    label: 'Community',
+    url: '/commmunity'
+  }, {
+    label: 'Manage process or tool'
+  }]
+  selectedState = 'South Dakota';
+
+  search1 = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      map((term: string) => states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1))
+    )
+
+  itemOnSelected1 = (item: string) => {
+    console.log(item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ClarityModule } from "clarity-angular";
 import { LibModule } from 'quickstart-lib';
+import { PrometheusWebComponentsModule } from 'prometheus-web-components';
 
 import { AppComponent } from './app.component';
 
@@ -12,8 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ClarityModule.forRoot(),
-    LibModule
+    LibModule,
+    PrometheusWebComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
